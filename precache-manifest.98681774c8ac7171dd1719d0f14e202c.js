4d9454fa5755c98d470781d54c6346fd0959cfdd
Sivareddymedapati <sivareddymedapati@gmail.com>
2020-05-13 16:42:37 +0530

    Updates

---
+++ precache-manifest.98681774c8ac7171dd1719d0f14e202c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab259a7ce844256314c3f60b16c70e3",
+    "url": "/recipeSearch/index.html"
+  },
+  {
+    "revision": "dea0af311443c9cc778d",
+    "url": "/recipeSearch/static/css/main.4a59ba91.chunk.css"
+  },
+  {
+    "revision": "84bcdd9c83d2fcf70b0f",
+    "url": "/recipeSearch/static/js/2.2582197a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/recipeSearch/static/js/2.2582197a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea0af311443c9cc778d",
+    "url": "/recipeSearch/static/js/main.f425bc75.chunk.js"
+  },
+  {
+    "revision": "1e0a96faab8b6a8cd3db",
+    "url": "/recipeSearch/static/js/runtime-main.97351340.js"
+  },
+  {
+    "revision": "0355196bc9ea5aaa4e07e1802673c3af",
+    "url": "/recipeSearch/static/media/display1.0355196b.jpg"
+  }
+]);
